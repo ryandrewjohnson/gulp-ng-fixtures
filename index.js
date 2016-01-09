ee09e483f0f7b450195e9807b15da3c752e99998
ryanjohnson <ryan@shakainteractive.com>
2016-01-08 19:02:59 -0500

    initial source files

---
+++ index.js
@@ -0,0 +1,241 @@
+/**
+ * What I need to know from the user:
+ * - the index.html file for app
+ * - the main app module name
+ * - a config with http req and res data
+ *
+ * What I need to output?
+ * - a script tag with angular mocks and the new module
+ * - generating angular service and injecting it
+ *
+ * TODO:
+ * - add gulp logging and error handling
+ * - throw error if isStream
+ * - add error handling for if JSON file not found
+ * - move to isolated project and rename everything - DONE
+ * - add comments
+ * - write tests - WIP
+ */
+
+'use strict';
+
+var fs          = require('fs');
+var through     = require('through2');
+var gutil       = require('gulp-util');
+var PluginError = gutil.PluginError;
+var Rx          = require('rx');
+var handlebars  = require('handlebars');
+
+var defaults,
+    readFile,
+    HTTP_METHODS;
+
+defaults = {
+  angularMocksUrl: '//cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.0-beta.2/angular-mocks.js',
+  method: 'get',
+  status: 200
+};
+
+HTTP_METHODS = [
+  'get',
+  'post',
+  'put',
+  'delete',
+  'head'
+];
+
+readFile = Rx.Observable.fromNodeCallback(fs.readFile);
+
+/*
+var opts = {
+  appModule: '',
+  fixtures: [
+    {
+      req: '',
+      res: 'either a path or object',
+      statusCode: '',
+      method: 'get,post,etc..'
+    }
+  ],
+  ignore: [
+    ''
+  ]
+};
+*/
+
+
+module.exports = function(opts) {
+  var options = merge_options(defaults, opts);
+
+  if (typeof opts === 'string') {
+    return removeFixtures(opts);
+  }
+
+  return addFixtures(options);
+};
+
+
+function addFixtures(opts) {
+  var fixturesStream,
+      readTplStream;
+
+  if (!opts.appModule) {
+    throw new PluginError('gulp-ng-fixtures', 'appModule option is required');
+  }
+
+  gutil.log('Add fixtures for app module', gutil.colors.magenta(opts.appModule));
+
+  fixturesStream = readFixtures(opts.fixtures);
+  readTplStream  = readFile('./template.html');
+
+  return through.obj(function(file, encoding, callback) {
+    var stream = fixturesStream
+      .concat(readTplStream)
+      .concat(removeAppFromIndex(file, opts.appModule))
+      .toArray();
+
+    stream.subscribe(function(results) {
+      var contents = parseIndexFile(results, opts);
+      file.contents = new Buffer(contents);
+      callback(null, file);
+    });
+  });
+}
+
+
+function removeFixtures(appModule) {
+  gutil.log('Remove fixtures for app module', gutil.colors.magenta(appModule));
+
+  return through.obj(function(file, encoding, callback) {
+    var contents = file.contents.toString();
+
+    contents = contents.replace(/^<!-- ng:fixtures -->(.|\n)*<!-- endfixtures -->$/gmi, '');
+    contents = contents.replace('data-ng-fixtures="'+ appModule +'"', 'ng-app="'+ appModule +'"');
+
+    file.contents = new Buffer(contents);
+    callback(null, file);
+  });
+}
+
+
+
+function parseIndexFile(results, opts) {
+  var fixtures    = results[0],
+      template    = results[1],
+      indexFile   = results[2],
+      scriptsTpl,
+      hbOptions;
+
+  hbOptions = {
+    appmodule:    opts.appModule,
+    angularmocks: opts.angularMocksUrl,
+    fixtures:     fixtures
+  };
+
+  // render scripts template inserting fixture data
+  scriptsTpl = handlebars.compile(template.toString());
+  scriptsTpl = scriptsTpl(hbOptions);
+
+  // insert scripts template into bottom of index.html
+  return insertTemplate(indexFile, scriptsTpl);
+}
+
+function removeAppFromIndex(file, appModule) {
+  return Rx.Observable.just(file)
+    .pluck('contents')
+    .map(function(contents) {
+      contents = contents.toString();
+      return removeNgApp(contents, appModule);
+    });
+}
+
+function readFixtures(fixtures) {
+  var fixturesStream,
+      jsonFixtures,
+      jsonReadFixtures,
+      jsonCombined,
+      otherFixtures;
+
+  fixturesStream = Rx.Observable.from(fixtures)
+    .map(function(fixture) {
+      fixture.method = parseHttpMethod(fixture.method);
+      fixture.status = fixture.status || defaults.status;
+      return fixture;
+    });
+
+  // filter out fixtures that are json files
+  jsonFixtures = fixturesStream
+    .filter(function(fixture) {
+      return isJsonPath(fixture.res);
+    });
+
+  // for each json fixture read file data
+  jsonReadFixtures = jsonFixtures
+    .flatMap(function(fixture) {
+      return readFile(fixture.res);
+    })
+    .map(function(data, idx, obs) {
+      return data.toString();
+    });
+
+  // set each fixture.res equal to the json data returned from readfile
+  jsonCombined = jsonFixtures.zip(jsonReadFixtures, function(fixture, data) {
+    fixture.res = data;
+    return fixture;
+  });
+
+  // gather non-json fixture response types
+  otherFixtures = fixturesStream
+    .filter(function(fixture) {
+      return !isJsonPath(fixture.res);
+    });
+
+  // combine json and non-json fixtures
+  return jsonCombined.merge(otherFixtures).toArray();
+}
+
+function removeNgApp(html, appModule) {
+  return html.replace('ng-app="'+ appModule +'"', 'data-ng-fixtures="'+ appModule +'"');
+}
+
+function insertTemplate(html, tpl) {
+  var tpl = tpl + '</body>';
+  return html.replace('</body>', tpl);
+}
+
+function isJsonPath(value) {
+  if ((typeof value === 'string') === false) { return false; }
+  return (value.indexOf('.json') >= 0);
+}
+
+function parseHttpMethod(value) {
+  var method = defaults.method;
+
+  if (value === undefined) { return method; }
+
+  HTTP_METHODS.some(function(verb) {
+    if (verb === value.toString().toLowerCase()) {
+      method = verb;
+      return true;
+    }
+  });
+
+  return method;
+}
+
+function merge_options(obj1, obj2){
+    var obj3 = {};
+    for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
+    for (var attrname in obj2) { obj3[attrname] = obj2[attrname]; }
+    return obj3;
+}
+
+handlebars.registerHelper('json', function(context) {
+    return JSON.stringify(context);
+});
+
+handlebars.registerHelper('parseReq', function(context) {
+    if (context instanceof RegExp) { return context; }
+    return '"' + context + '"';
+});
+
---
+++ test/index.js
@@ -0,0 +1,158 @@
+'use strict';
+
+var should      = require('chai').should();
+var gutil       = require('gulp-util');
+var PluginError = gutil.PluginError;
+var ngFixtures  = require('../');
+var fs          = require('fs');
+var File        = require('gulp-util').File;
+var gulp        = require('gulp');
+var assert      = require('stream-assert');
+var dummyJson   = require('../test/fixtures/dummy.json');
+
+describe('gulp-ng-fixtures', function() {
+  var paths,
+      options,
+      mockFixtures;
+
+  paths = {
+    inputHtml: 'test/fixtures/input.html'
+  };
+
+  options = {
+    appModule: 'myapp',
+    fixtures: []
+  };
+
+  mockFixtures = [
+    {
+      req: '/api/call/fake',
+      res: 'success',
+      method: 'post'
+    },
+    {
+      req: /\/api\/call\/fake/,
+      method: 'put',
+      res: {test: 'this'}
+    },
+    {
+      req: '/api/call/fake/200',
+      status: 401,
+      res: 200
+    },
+    {
+      req: 'test/json/response',
+      res: 'test/fixtures/dummy.json'
+    },
+    {req: '/api/tester'}
+  ];
+
+  beforeEach(function() {
+  });
+
+  it('should throw error missing appModule option', function() {
+    (function() {
+      ngFixtures();
+    })
+    .should.throw(/appModule option is required/);
+  });
+
+  it('should remove ng-app attribute and replace with custom attr', function(done) {
+    gulp.src(paths.inputHtml)
+    .pipe(ngFixtures(options))
+    .pipe(assert.first(function(d) {
+      d.contents.toString().should.not.match(/ng-app="myapp"/);
+      d.contents.toString().should.match(/data-ng-fixtures="myapp"/);
+    }))
+    .pipe(assert.end(done));
+  });
+
+  it('should insert default ngMocks script tag', function(done) {
+    gulp.src(paths.inputHtml)
+    .pipe(ngFixtures(options))
+    .pipe(assert.first(function(d) {
+      d.contents.toString().should.match(/<script src="\/\/cdnjs.cloudflare.com\/ajax\/libs\/angular.js\/1.5.0-beta.2\/angular-mocks.js">/);
+    }))
+    .pipe(assert.end(done));
+  });
+
+  it('should override default ngMocks script tag', function(done) {
+    options.angularMocksUrl = '/local/angular-mocks.js';
+
+    gulp.src(paths.inputHtml)
+    .pipe(ngFixtures(options))
+    .pipe(assert.first(function(d) {
+      d.contents.toString().should.match(/<script src="\/local\/angular-mocks.js">/);
+    }))
+    .pipe(assert.end(done));
+  });
+
+  it('should add fixtures js before <body> tag', function(done) {
+    options.angularMocksUrl = '/local/angular-mocks.js';
+
+    gulp.src(paths.inputHtml)
+    .pipe(ngFixtures(options))
+    .pipe(assert.first(function(d) {
+      d.contents.toString().should.match(/<!-- ng:fixtures -->(.|\n)*<!-- endfixtures -->\n<\/body>/gmi);
+      d.contents.toString().should.match(/module\('fixtures\.app', \['myapp', 'fixtures\.service'\]\)/);
+    }))
+    .pipe(assert.end(done));
+  });
+
+  it.only('should add $httpBackend call for each fixture item', function(done) {
+    var pattern = getFixtureRegex(mockFixtures);
+
+    options.fixtures = mockFixtures;
+
+    console.log('pattern', pattern);
+
+    gulp.src(paths.inputHtml)
+    .pipe(ngFixtures(options))
+    .pipe(assert.first(function(d) {
+      d.contents.toString().should.match(new RegExp(pattern, 'gmi'));
+    }))
+    .pipe(assert.end(done));
+  });
+
+});
+
+function getFixtureRegex(fixtures) {
+  var tmpl1 = '$httpBackend.when({url}, "{method}").respond({status}, {res});';
+  var tmpl2 = '$httpBackend.whenGET({url}).passThrough();';
+  var match = '';
+
+
+  fixtures.forEach(function(item) {
+    var req = typeof item.req === 'string' ? '"' + item.req + '"' : item.req;
+    var result = '';
+
+    item.method = item.method || 'get';
+    item.status = item.status || 200;
+
+    if (item.res) {
+      var res = (typeof item.res === 'string' && item.res.indexOf('.json') >= 0) ?
+        JSON.stringify("{\"id\": 1,\"user\": \"testymctest\"}") :
+        JSON.stringify(item.res);
+
+      result += tmpl1.replace(/\{url\}/, req)
+                    .replace(/\{method\}/, item.method)
+                    .replace(/\{status\}/, item.status)
+                    .replace(/\{res\}/, res);
+    }
+    else {
+      result += tmpl2.replace(/\{url\}/, req);
+    }
+
+    match += escapeRegExp(result) + '(.|\n)*';
+  });
+
+  return match;
+}
+
+function escapeRegExp(str) {
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+}
+
+
+
+
